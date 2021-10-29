e2a7eaf7aef127f2e6a9cf089f3a8444e13e578e
jackie35er <jackie35er@gmail.com>
2021-10-29 21:09:10 +0200

    NumberValidators

---
+++ src/main/java/util/DoubleValidator.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.util.function.ToDoubleFunction;
+
+public class DoubleValidator<T> extends Validator<T,Double> {
+
+    protected DoubleValidator(ToDoubleFunction<T> keyExtractor) {
+        super(keyExtractor::applyAsDouble);
+    }
+
+    protected DoubleValidator(ToDoubleFunction<T> keyExtractor, IValidator<T> validator) {
+        super(keyExtractor::applyAsDouble, validator);
+    }
+
+    public DoubleValidator<T> inRange(double minInclusive, double maxExclusive){
+        super.thenVaildating(n -> minInclusive <= n && n < maxExclusive);
+        return this;
+    }
+
+    public DoubleValidator<T> inRangeExclusive(double minExclusive, double maxExclusive){
+        super.thenVaildating(n -> minExclusive <= n && n < maxExclusive);
+        return this;
+    }
+
+    public DoubleValidator<T> inRangeInclusive(double minInclusive, double maxInclusive){
+        super.thenVaildating(n -> minInclusive <= n && n < maxInclusive);
+        return this;
+    }
+
+    public DoubleValidator<T> bigger(double value){
+        super.thenVaildating(n -> n > value);
+        return this;
+    }
+
+    public DoubleValidator<T> smaller(double value){
+        super.thenVaildating(n -> n < value);
+        return this;
+    }
+
+
+    public DoubleValidator<T> positiv(){
+        return this.bigger(0);
+    }
+
+    public DoubleValidator<T> negativ(){
+        return this.smaller(0);
+    }
+
+}
---
+++ src/main/java/util/LongValidator.java
@@ -0,0 +1,50 @@
+package util;
+
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
+public class LongValidator<T> extends Validator<T,Long> {
+
+    protected LongValidator(ToLongFunction<T> keyExtractor) {
+        super(keyExtractor::applyAsLong);
+    }
+
+    protected LongValidator(ToLongFunction<T> keyExtractor, IValidator<T> validator) {
+        super(keyExtractor::applyAsLong, validator);
+    }
+
+    public LongValidator<T> inRange(int minInclusive, int maxExclusive){
+        super.thenVaildating(n -> minInclusive <= n && n < maxExclusive);
+        return this;
+    }
+
+    public LongValidator<T> inRangeExclusive(int minExclusive, int maxExclusive){
+        super.thenVaildating(n -> minExclusive <= n && n < maxExclusive);
+        return this;
+    }
+
+    public LongValidator<T> inRangeInclusive(int minInclusive, int maxInclusive){
+        super.thenVaildating(n -> minInclusive <= n && n < maxInclusive);
+        return this;
+    }
+
+    public LongValidator<T> bigger(int value){
+        super.thenVaildating(n -> n > value);
+        return this;
+    }
+
+    public LongValidator<T> smaller(int value){
+        super.thenVaildating(n -> n < value);
+        return this;
+    }
+
+
+    public LongValidator<T> positiv(){
+        return this.bigger(0);
+    }
+
+    public LongValidator<T> negativ(){
+        return this.smaller(0);
+    }
+
+}
